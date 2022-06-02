d6e226770f50134dbc3fc3ddf2f4bf8eff78a466
Panachai <panachai.ny@gmail.com>
2022-06-02 23:29:36 +0700

    add O turn

--- src/index.jsx
+++ src/index.jsx
@@ -15,13 +15,14 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {
@@ -34,11 +35,10 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
-        <div>test: {this.state.squares}</div>
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
